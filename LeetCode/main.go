2d21807d4aacd663ddac8a5e16f6724fd25b1a9b
eric-lab-star <cyon256@icloud.com>
2023-03-27 13:24:38 +0900

    Remove Elements
    my solution

--- LeetCode/main.go
+++ LeetCode/main.go
@@ -6,10 +6,31 @@ import "fmt"
 
 func main() {
 	input := []int{3, 2, 2, 3}
-	k := removeElements(input, 3)
-	fmt.Print(k)
+	k := removeElement(input, 3)
+	fmt.Println(k)
 }
 
-func removeElements(nums []int, val int) int {
-	return len(nums)
+func removeElement(nums []int, val int) int {
+	i := 0
+	j := len(nums) - 1
+	for i <= j {
+		if nums[i] == val {
+			nums[i] = 0
+			nums[i], nums[j] = nums[j], nums[i]
+			j--
+		}
+		if nums[i] != val {
+			i++
+		}
+	}
+	fmt.Println(nums, i, j)
+	if j+2 == i {
+		return i - 1
+	}
+	return i
 }
+
+// [2,3]
+// [3]
+// [3,2]
+// []
