eaab6be5f88bdc125b44008e49da9ee20bfc2aa2
eric-lab-star <cyon256@icloud.com>
2023-03-27 11:04:03 +0900

    p27

--- LeetCode/main.go
+++ LeetCode/main.go
@@ -5,5 +5,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("heelo")
+	input := []int{3, 2, 2, 3}
+	k := removeElements(input, 3)
+	fmt.Print(k)
+}
+
+func removeElements(nums []int, val int) int {
+	return len(nums)
 }
