ad5b870a29d0a56a027bdf20a2900acad54d9656
eric-lab-star <cyon256@icloud.com>
2023-03-27 15:22:12 +0900

    p28 setup

--- LeetCode/main.go
+++ LeetCode/main.go
@@ -1,31 +1,11 @@
-//Remove Element
+//Find the index of the first Occurence in a String
 
 package main
 
-import "fmt"
-
 func main() {
-	input := []int{3, 2, 2, 3}
-	k := removeElement(input, 3)
-	fmt.Println(k)
+
 }
 
-func removeElement(nums []int, val int) int {
-	i := 0
-	j := len(nums) - 1
-	for i <= j {
-		if nums[i] == val {
-			nums[i] = 0
-			nums[i], nums[j] = nums[j], nums[i]
-			j--
-		}
-		if nums[i] != val {
-			i++
-		}
-	}
-	fmt.Println(nums, i, j)
-	if j+2 == i {
-		return i - 1
-	}
-	return i
+func strStr(haystack string, needle string) int {
+
 }
