a23e60ed75fb2e4b6c98ade735a955ede2a7abd5
eric-lab-star <cyon256@icloud.com>
2023-04-06 16:22:36 +0900

    go concurrency patterns

--- chi-go/main.go
+++ chi-go/main.go
@@ -2,73 +2,53 @@ package main
 
 import (
 	"fmt"
-	"sync"
+	"math/rand"
+	"time"
 )
 
-// converts list of int to channel that emits the integers in the list.
-func gen(nums ...int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for _, n := range nums {
-			out <- n
-		}
-		//close channel when all the values have been sent
-		close(out)
-	}()
-	return out
-}
-
-// receive ints from channel and returns a channel that emits the square of each received integer.
-func sq(in <-chan int) <-chan int {
-	out := make(chan int)
-	go func() {
-		for n := range in {
-			out <- n * n
-		}
-		// after the inbound channel is closed and this stage has sent all the values downstream, it closes the outbound channel
-		close(out)
-	}()
-	return out
-}
-
-/*
-* converts a lists of channels to a single channel by starting a goroutine for each inbound channel that copies the values to the sole
-* out bound channel.
- */
-func merge(cs ...<-chan int) <-chan int {
-	var wg sync.WaitGroup
-	out := make(chan int)
-
-	// Start an output goroutine for each input channel in cs.  output
-	// copies values from c to out until c is closed, then calls wg.Done.
-	output := func(c <-chan int) {
-		for n := range c {
-			out <- n
-		}
-		wg.Done()
-	}
-	wg.Add(len(cs))
-	for _, c := range cs {
-		go output(c)
-	}
-
-	// Start a goroutine to close out once all the output goroutines are
-	// done.  This must start after the wg.Add call.
-	go func() {
-		wg.Wait()
-		close(out)
-	}()
-	return out
-}
 func main() {
-	in := gen(2, 3)
-
-	// Distribute the sq work across two goroutines that both read from in.
-	c1 := sq(in)
-	c2 := sq(in)
-
-	// Consume the merged output from c1 and c2.
-	for n := range merge(c1, c2) {
-		fmt.Println(n) // 4 then 9, or 9 then 4
+	c := fanIn(boring("jon:"), boring("ann:"))
+	for i := 0; i < 10; i++ {
+		msg1 := <-c
+		fmt.Println(msg1.str)
+		msg2 := <-c
+		fmt.Println(msg2.str)
+		msg1.wait <- true
+		msg2.wait <- true
 	}
+
+	fmt.Println("you are boring. I am leaving.")
+}
+
+func boring(msg string) <-chan message {
+	c := make(chan message)
+	waitforit := make(chan bool)
+	go func() {
+		for i := 0; ; i++ {
+			c <- message{fmt.Sprintf("%s %d", msg, i), waitforit}
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			<-waitforit
+		}
+	}()
+	return c
+}
+
+func fanIn(jon, ann <-chan message) <-chan message {
+	c := make(chan message)
+	go func() {
+		for {
+			c <- <-jon
+		}
+	}()
+	go func() {
+		for {
+			c <- <-ann
+		}
+	}()
+	return c
+}
+
+type message struct {
+	str  string
+	wait chan bool
 }
