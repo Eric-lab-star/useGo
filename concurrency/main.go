3ea8539dc35239049ad9e10ccfd4a215cee17950
eric-lab-star <cyon256@icloud.com>
2023-04-13 16:24:27 +0900

    echo server

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,36 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net"
+)
+
+const listenAddr = "localhost:4000"
 
 func main() {
-	data := []int{1, 2, 3, 4, 5, 6}
-	list1 := []int{}
-	list2 := []int{}
-	c1 := make(chan bool)
-	c2 := make(chan bool)
-	c3 := make(chan bool)
-	go func() {
 
-		go func() {
-			for _, v := range data {
-				list1 = append(list1, v)
-			}
-			c1 <- true
-		}()
-		go func() {
-			for _, v := range data {
-				list2 = append(list2, v)
-			}
-			c2 <- true
-		}()
-		if <-c1 && <-c2 {
-			c3 <- true
+	l, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
-	select {
-	case <-c3:
-		fmt.Println(list1)
-		fmt.Println(list2)
+
+		io.Copy(c, c)
 	}
 
 }
