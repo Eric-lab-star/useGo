dd28b3eeee0ccee5872d99917843abded52fde3b
eric-lab-star <cyon256@icloud.com>
2023-04-14 22:11:20 +0900

    ping pong

--- concurrency/main.go
+++ concurrency/main.go
@@ -2,110 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-	"text/template"
-
-	"golang.org/x/net/websocket"
+	"time"
 )
 
-const listenAddr = "localhost:4000"
+type Ball struct{ hits int }
 
 func main() {
-	http.HandleFunc("/", rootHandler)
-	http.Handle("/socket", websocket.Handler(socketHandler))
-	err := http.ListenAndServe(listenAddr, nil)
-	if err != nil {
-		log.Fatal(err)
+	table := make(chan *Ball)
+	go player("3", table)
+	go player("1", table)
+	go player("2", table)
+	table <- new(Ball)
+	time.Sleep(1 * time.Second)
+	<-table
+}
+
+func player(name string, table chan *Ball) {
+	for {
+		ball := <-table
+		ball.hits++
+		fmt.Println(name, ball.hits)
+		time.Sleep(100 * time.Millisecond)
+		table <- ball
 	}
-
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	rootTemplate.Execute(w, listenAddr)
-}
-
-var rootTemplate = template.Must(template.New("root").Parse(`
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<body>
-<form id="form">
-<input id="msg" type="text" placeholder="your message"/>
-</form>
-</body>
-<script>
-	var form = document.getElementById("form")
-	var websocket = new WebSocket("ws://{{.}}/socket");
-	const input = document.querySelector("input")
-	function onSubmit(event){
-		event.preventDefault()
-		const msg = input.value
-		const h1 = document.createElement("h1");
-		document.body.appendChild(h1);
-		h1.innerText = "Me: " + msg
-		websocket.send("Partner:" + msg)
-		input.value = ""
-	}
-	form.addEventListener("submit", onSubmit)
-    websocket.onmessage = function(m){
-		var h1 = document.createElement("h1");
-		document.body.appendChild(h1);
-		h1.innerText = m.data;
-	}
-    websocket.onclose = onClose;
-	function onClose(){
-		var h1 = document.createElement("h1");
-		document.body.appendChild(h1);
-		h1.innerText = "user left";
-	}
-</script>
-</html>
-`))
-
-type socket struct {
-	io.ReadWriter
-	done chan bool
-}
-
-func (s socket) Close() error {
-	s.done <- true
-	return nil
-}
-
-func socketHandler(ws *websocket.Conn) {
-	s := socket{ws, make(chan bool)}
-	go match(s)
-	<-s.done
-}
-
-var partner = make(chan io.ReadWriteCloser)
-
-func match(c io.ReadWriteCloser) {
-	fmt.Fprint(c, "Waiting for a partner...")
-	select {
-	case partner <- c:
-		// now handled by the other goroutine
-	case p := <-partner:
-		chat(p, c)
-	}
-}
-
-func chat(a, b io.ReadWriteCloser) {
-	fmt.Fprintln(a, "Found one! Say hi.")
-	fmt.Fprintln(b, "Found one! Say hi.")
-	errc := make(chan error, 1)
-	go cp(a, b, errc)
-	go cp(b, a, errc)
-	if err := <-errc; err != nil {
-		log.Println(err)
-	}
-	a.Close()
-	b.Close()
-}
-func cp(w io.Writer, r io.Reader, errc chan<- error) {
-	_, err := io.Copy(w, r)
-	errc <- err
 }
