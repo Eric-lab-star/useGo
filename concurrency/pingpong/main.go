12f6973e289c42da66cf5c881abf7a93471de5b8
eric-lab-star <cyon256@icloud.com>
2023-04-20 16:52:36 +0900

    pprof

--- concurrency/pingpong/main.go
+++ concurrency/pingpong/main.go
@@ -2,28 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/pprof"
 	"time"
 )
 
-func main() {
+func main() { //main function is goroutine
 
-	table := make(chan *Ball)
-	go player("ping", table)
+	table := make(chan *Ball) // unbuffered channel
+	go player("ping", table)  // execute goroutine
 	go player("pong", table)
 	table <- new(Ball)
 	time.Sleep(1 * time.Second)
-	panic("show goroutines")
+
+	pprof.Lookup("goroutine").WriteTo(os.Stdout, 1) //print gorountine
 }
 
 type Ball struct{ hit int }
 
 func player(name string, table chan *Ball) {
+
 	for {
 		ball := <-table
 		ball.hit++
 		fmt.Printf("%s hits: %d\n", name, ball.hit)
 		time.Sleep(100 * time.Millisecond)
+		fmt.Println(name, "ball received")
 		table <- ball
+		fmt.Println(name, "ball sent")
 	}
 
 }
